a7fd6a3d3985fed2078b41a1a5693816d7f8386e
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-02-16 14:23:55 +0100

    upgrade 'alumbra/claro' dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :year 2016
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
-                 [alumbra/claro "0.1.1"]
+                 [alumbra/claro "0.1.2"]
                  [alumbra/ring "0.1.0-SNAPSHOT"]
                  [alumbra/analyzer "0.1.6"]
                  [alumbra/validator "0.1.0-SNAPSHOT"]
