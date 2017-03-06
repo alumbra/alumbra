3efbf2bba5a69ac4296b8ec223ce11ac49c9971a
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-03-06 19:04:08 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [alumbra/claro "0.1.4"]
-                 [alumbra/ring "0.1.0-SNAPSHOT"]
-                 [alumbra/analyzer "0.1.7"]
+                 [alumbra/ring "0.1.0"]
+                 [alumbra/analyzer "0.1.8"]
                  [alumbra/validator "0.1.0"]
                  [alumbra/parser "0.1.5"]]
   :profiles {:example
