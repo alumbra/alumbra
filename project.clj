a9febe5388f7cca84d2391a3db5e04e4fd7075cb
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-03-20 10:04:55 +0100

    add 'codox' plugin.

--- project.clj
+++ project.clj
@@ -11,7 +11,17 @@
                  [alumbra/analyzer "0.1.9"]
                  [alumbra/validator "0.1.1"]
                  [alumbra/parser "0.1.6"]]
-  :profiles {:dev {:dependencies [[aleph "0.4.3"]
-                                  [riddley "0.1.14"]
-                                  [clj-http "3.4.1"]]}}
+  :profiles {:dev
+             {:dependencies [[aleph "0.4.3"]
+                             [riddley "0.1.14"]
+                             [clj-http "3.4.1"]]}
+             :codox
+             {:plugins [[lein-codox "0.10.3"]]
+              :dependencies [[codox-theme-rdash "0.1.1"]]
+              :codox {:project {:name "alumbra"}
+                      :metadata {:doc/format :markdown}
+                      :themes [:rdash]
+                      :source-uri "https://github.com/alumbra/alumbra/blob/v{version}/{filepath}#L{line}"
+                      :namespaces [alumbra.core]}}}
+  :aliases {"codox" ["with-profile" "+codox" "codox"]}
   :pedantic? :abort)
