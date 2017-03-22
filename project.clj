a23bc6029647fd7ec80fd650b199ebbdb75d47ba
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-03-22 11:02:05 +0100

    upgrade 'alumbra/ring' dependency.
    Fixes #18.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [alumbra/claro "0.1.5"]
-                 [alumbra/ring "0.1.1"]
+                 [alumbra/ring "0.1.2"]
                  [alumbra/analyzer "0.1.9"]
                  [alumbra/validator "0.1.1"]
                  [alumbra/parser "0.1.6"]]
