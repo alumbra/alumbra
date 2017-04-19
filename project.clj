115ff688fa6ac357bb23b29d5ebd2c46fb15d82d
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-04-19 14:06:58 +0200

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra "0.1.2-SNAPSHOT"
+(defproject alumbra "0.1.2"
   :description "Simple and Elegant GraphQL for Clojure"
   :url "https://github.com/alumbra/alumbra"
   :license {:name "MIT License"
